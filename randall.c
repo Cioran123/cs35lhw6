f967e119848de2c5928f070a564a660c4135eadb
Cioran123 <119903481+Cioran123@users.noreply.github.com>
2024-11-26 23:06:24 -0800

    commit

--- randall.c
+++ randall.c
@@ -30,177 +30,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Hardware implementation.  */
 
-/* Description of the current CPU.  */
-struct cpuid { unsigned eax, ebx, ecx, edx; };
-
-/* Return information about the CPU.  See <http://wiki.osdev.org/CPUID>.  */
-static struct cpuid
-cpuid (unsigned int leaf, unsigned int subleaf)
-{
-  struct cpuid result;
-  asm ("cpuid"
-       : "=a" (result.eax), "=b" (result.ebx),
-	 "=c" (result.ecx), "=d" (result.edx)
-       : "a" (leaf), "c" (subleaf));
-  return result;
-}
-
-/* Return true if the CPU supports the RDRAND instruction.  */
-static _Bool
-rdrand_supported (void)
-{
-  struct cpuid extended = cpuid (1, 0);
-  return (extended.ecx & bit_RDRND) != 0;
-}
-
-/* Initialize the hardware rand64 implementation.  */
-static void
-hardware_rand64_init (void)
-{
-}
-
-/* Return a random value, using hardware operations.  */
-static unsigned long long
-hardware_rand64 (void)
-{
-  unsigned long long int x;
-
-  /* Work around GCC bug 107565
-     <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=107565>.  */
-  x = 0;
-
-  while (! _rdrand64_step (&x))
-    continue;
-  return x;
-}
-
-/* Finalize the hardware rand64 implementation.  */
-static void
-hardware_rand64_fini (void)
-{
-}
+#include "options.h"
+#include "output.h"
+#include "rand64-hw.h"
+#include "rand64-sw.h"
 
 
-
-/* Software implementation.  */
-
-/* Input stream containing random bytes.  */
-static FILE *urandstream;
-
-/* Initialize the software rand64 implementation.  */
-static void
-software_rand64_init (void)
-{
-  urandstream = fopen ("/dev/random", "r");
-  if (! urandstream)
-    abort ();
-}
-
-/* Return a random value, using software operations.  */
-static unsigned long long
-software_rand64 (void)
-{
-  unsigned long long int x;
-  if (fread (&x, sizeof x, 1, urandstream) != 1)
-    abort ();
-  return x;
-}
-
-/* Finalize the software rand64 implementation.  */
-static void
-software_rand64_fini (void)
-{
-  fclose (urandstream);
-}
-
-static bool
-writebytes (unsigned long long x, int nbytes)
-{
-  do
-    {
-      if (putchar (x) < 0)
-	return false;
-      x >>= CHAR_BIT;
-      nbytes--;
-    }
-  while (0 < nbytes);
-
-  return true;
-}
+#define MIN(x, y) (x) < (y) ? (x) : (y)
 
 /* Main program, which outputs N bytes of random data.  */
-int
-main (int argc, char **argv)
-{
-  /* Check arguments.  */
-  bool valid = false;
-  long long nbytes;
-  if (argc == 2)
-    {
-      char *endptr;
-      errno = 0;
-      nbytes = strtoll (argv[1], &endptr, 10);
-      if (errno)
-	perror (argv[1]);
-      else
-	valid = !*endptr && 0 <= nbytes;
-    }
-  if (!valid)
-    {
-      fprintf (stderr, "%s: usage: %s NBYTES\n", argv[0], argv[0]);
-      return 1;
+int main(int argc, char **argv) {
+    bool rdrand = false;
+    bool mrand48 = false;
+    long long nbytes;
+    
+    //The maximum combined length = 4096 bytes.
+    char filename[4096];
+    int N;
+    bool doGroup = false;
+
+    int status =
+        options(argc, argv, &rdrand, &mrand48, &nbytes, filename, &N, &doGroup);
+
+    if (status) {
+        return status;
     }
 
-  /* If there's no work to do, don't worry about which library to use.  */
-  if (nbytes == 0)
-    return 0;
-
-  /* Now that we know we have work to do, arrange to use the
-     appropriate library.  */
-  void (*initialize) (void);
-  unsigned long long (*rand64) (void);
-  void (*finalize) (void);
-  if (rdrand_supported ())
-    {
-      initialize = hardware_rand64_init;
-      rand64 = hardware_rand64;
-      finalize = hardware_rand64_fini;
-    }
-  else
-    {
-      initialize = software_rand64_init;
-      rand64 = software_rand64;
-      finalize = software_rand64_fini;
+    /* If there's no work to do, don't worry about which library to use.  */
+    if (nbytes == 0) {
+        return 0;
     }
 
-  initialize ();
-  int wordsize = sizeof rand64 ();
-  int output_errno = 0;
+    /* Now that we know we have work to do, arrange to use the
+       appropriate library.  */
+    // void (*initialize)(void);
 
-  do
-    {
-      unsigned long long x = rand64 ();
-      int outbytes = nbytes < wordsize ? nbytes : wordsize;
-      if (!writebytes (x, outbytes))
-	{
-	  output_errno = errno;
-	  break;
-	}
-      nbytes -= outbytes;
-    }
-  while (0 < nbytes);
+    unsigned long long (*rand64)(void);
+    void (*finalize)(void);
 
-  if (fclose (stdout) != 0)
-    output_errno = errno;
-
-  if (output_errno)
-    {
-      errno = output_errno;
-      perror ("output");
+    if (rdrand) {
+        if (rdrand_supported()) {
+            hardware_rand64_init();
+            rand64 = hardware_rand64;
+            finalize = hardware_rand64_fini;
+        } else {
+            fprintf(stderr, "ERROR: rdrand not supported\n");
+            return 1;
+        }
+    } else {
+        software_rand64_init(filename, mrand48);
+        rand64 = software_rand64;
+        finalize = software_rand64_fini;
     }
 
-  finalize ();
-  return !!output_errno;
+    // initialize();
+
+    int output_errno = 0;
+
+    if (!doGroup) {
+        int wordsize = sizeof rand64();
+
+        do {
+            unsigned long long x = rand64();
+            int outbytes = nbytes < wordsize ? nbytes : wordsize;
+            if (!writebytes(x, outbytes)) {
+                output_errno = errno;
+                break;
+            }
+            nbytes -= outbytes;
+        } while (0 < nbytes);
+
+        if (fclose(stdout) != 0) output_errno = errno;
+
+        if (output_errno) {
+            errno = output_errno;
+            perror("output");
+        }
+    }
+
+    else {
+        char *buf = (char *)malloc(nbytes);
+
+        if (buf != NULL) {
+            unsigned long long x;
+            char rand_word[8];
+            for (int i = 0; i < nbytes; i += 8) {
+                x = rand64();
+                generate_random_word(x, rand_word);
+                memcpy(buf + i, rand_word, MIN(8, nbytes - i));
+            }
+            groupings(buf, nbytes, N);
+            free(buf);
+        }
+    }
+
+    finalize();
+    return !!output_errno;
 }
